054a8debc47036742b963f8bb7b48708bfcd6696
Himanshu Kumar <himanshukumar077@gmail.com>
2024-02-29 10:58:46 +0530

    Application main added

--- application-framework/src/index.jsx
+++ application-framework/src/index.jsx
@@ -6,6 +6,6 @@ export { default as Button } from "./components/Button";
 // import "./examples/App";
 
 // Configured using the env variables
-if (process.env.ENABLE_REACT_APP) {
-  require("./examples/App");
-}
+// if (process.env.ENABLE_REACT_APP) {
+//   require("./examples/App");
+// }
---
+++ application-main/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@himanshu077/application-framework";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button>Hello World</Button>
+    </>
+  );
+}
+
+export default App;
