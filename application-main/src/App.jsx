bf1f6d99d99ec80ee8886ae1746d9ac50e0b1480
Himanshu Kumar <himanshukumar077@gmail.com>
2024-03-01 16:18:44 +0530

    Added the background color and the commands to run the project using single command

--- application-main/src/App.jsx
+++ application-main/src/App.jsx
@@ -2,13 +2,31 @@ import { Button } from "@himanshu077/application-framework";
 import "./App.css";
 
 function App() {
+  const changeBackground = (variant) => {
+    const allVariants = {
+      warning: "linear-gradient(white, orange) orange",
+      success: "linear-gradient(white, green) green",
+      danger: "linear-gradient(white, red) red",
+      info: "linear-gradient(white, blue) blue",
+    };
+    // prepare code to set the background of body on the click of the button variant
+    document.body.style.background = allVariants[variant];
+  };
   return (
-    <>
-      <Button variant="danger">Danger Button</Button>
-      <Button variant="info">Info Button</Button>
-      <Button variant="success">Success Button</Button>
-      <Button variant="warning">Warning Button</Button>
-    </>
+    <div>
+      <Button variant="danger" onClick={() => changeBackground("danger")}>
+        Danger Button
+      </Button>
+      <Button variant="info" onClick={() => changeBackground("info")}>
+        Info Button
+      </Button>
+      <Button variant="success" onClick={() => changeBackground("success")}>
+        Success Button
+      </Button>
+      <Button variant="warning" onClick={() => changeBackground("warning")}>
+        Warning Button
+      </Button>
+    </div>
   );
 }
 
